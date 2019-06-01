1a1a623365f9f7dbf15b632e131b0a47fa67661d
hefang <he529564582@163.com>
2019-06-01 14:53:54 +0800

    整理code到icon.vue

--- src/icon.vue
+++ src/icon.vue
@@ -1,13 +1,15 @@
 <template>
-    
+    <svg class="g-icon" >
+        <use v-bind:xlink:href = "`#i-${name}`"></use>>
+    </svg> 
 </template>
 <script>
 export default {
-    
+   props:['name'] 
 }
 </script>
-<style>
-
+<style lang="scss">
+    .g-icon{width: 1em; height: 1em;}
 </style>
 
 
