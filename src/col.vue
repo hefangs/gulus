f3cfe6443ba62f9be4d4c5e90564b4d955fc538d
hefang <he529564582@163.com>
2019-06-05 23:18:42 +0800

    update col & row.vue

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]"
+     :style="{paddingLeft:gutter/2 +'px',paddingRight:gutter/2 +'px'}>
         <slot></slot>
     </div>
 </template>
@@ -9,11 +10,18 @@
         background: #00e0ff;
         width: 50%;
         border: 1px solid #155263;
+        padding: 0 10px;
 
         $class-prefix:col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n/24) * 100%
+            }
+        }
+        $class-prefix:offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24) * 100%
             }
         }
     }
@@ -22,7 +30,15 @@
     export default {
         name:'',
         props:{
-            span:[Number,String]
+            span:{
+                type:[Number,String]
+            },
+            offset:{
+                type:[Number,String]
+            },
+             gutter:{
+                type:[Number,String]
+            }
         }
     }
 </script>
--- src/row.vue
+++ src/row.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="row"></div>
+    <div class="row" :style="{marginLeft:-gutter/2 +'px',marginRight:-gutter/2 +'px'}">
+        <slot></slot>
+    </div>
 </template>
 <style lang="scss" scoped>
     .row{
         display: flex;
+        margin: -10px;
     }
 </style>
 <script>
-export default {
-    
-}
+    export default {
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
+    }
 </script>
 
