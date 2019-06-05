5881c6435f512e83a774020dfd5c03e9b543ee1b
hefang <he529564582@163.com>
2019-06-05 19:14:04 +0800

    add col.vue & row.vue雏形

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        background: #00e0ff;
+        width: 50%;
+        border: 1px solid #155263;
+
+        $class-prefix:col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24) * 100%
+            }
+        }
+    }
+</style>
+<script>
+    export default {
+        name:'',
+        props:{
+            span:[Number,String]
+        }
+    }
+</script>
+
+
