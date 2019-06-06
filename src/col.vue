065a9ad1237b3f6ea38c5fdd70f7b62737ab2e86
hefang <he529564582@163.com>
2019-06-06 19:45:29 +0800

    update col.vue

--- src/col.vue
+++ src/col.vue
@@ -66,6 +66,10 @@
 </script>
 <style scoped lang="scss">
   .col {
+    height: 100px;
+    background: #00e0ff;
+    width: 50%;
+    border: 1px solid #155263;   
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
