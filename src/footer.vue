70b283dfd4664d5703870c87b656cd2b20e1f2d1
hefang <he529564582@163.com>
2019-06-07 11:58:28 +0800

    add 基本功能完成

---
+++ src/footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="footer">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .footer{
+    background: #a1eafb;
+}
+</style>
+<script>
+export default {
+    
+}
+</script>
+
+
---
+++ src/layout.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="layout" :class="layoutClass">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .layout{
+        flex-grow: 1;
+        background: #00e0ff;
+        display: flex;
+        flex-direction: column;
+        border:burlywood 1px solid;
+        height: 350px;
+        &.hasSider{
+            flex-direction: row;
+        }
+    }
+</style>
+<script>
+export default {
+    name:'GuluLayout',
+    data(){
+        return{
+            layoutClass:{
+                hasSider:false
+            }
+        }
+    },
+    mounted(){
+        this.$children.forEach((vm) => {
+            if(vm.$options.name === 'GuluSider') {
+                this.layoutClass.hasSider = true
+            }
+        })
+    }
+}
+</script>
+
+
