cb539c18e93c239e2238ea81a6a31123f7f43d91
hefang <he529564582@163.com>
2019-06-08 18:16:32 +0800

    add position:top/middle/bottom 功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast" ref="wrapper">
+    <div class="toast" ref="wrapper" :class="toastClasses">
         <div class="message">
             <slot v-if="!enableHtml"></slot>
             <div v-else v-html="$slots.default[0]"></div>
@@ -32,14 +32,28 @@
                 enableHtml: {
                     type: Boolean,
                     default: false
+                },
+                position:{
+                    type:String,
+                    default:'top',
+                    validator(value){
+                        return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+                    }
                 }
             },
         created() {
-            console.log(this.closeButton);
+            // console.log(this.closeButton);
         },
         mounted() {
             this.execAutoClose()
-            this.updateStyle()     
+            this.updateStyle()   
+        },
+        computed:{
+            toastClasses(){
+                return{
+                    [`position-${this.position}`] : true
+                }
+            }
         },
         methods: {
             updateStyle() {
@@ -77,9 +91,7 @@
     line-height: 1.8;
     min-height: $toast-min-height;
     position: fixed;
-    top: 0;
     left: 50%;
-    transform: translateX(-50%);
     display: flex;
     align-items: center;
     border-radius: 4px;
@@ -98,6 +110,18 @@
         }
         .message{
             padding: 8px 0;
+        }
+        &.position-top{
+            top: 0;
+            transform: translateX(-50%)
+        }
+        &.position-bottom{
+            bottom: 0;
+            transform: translateX(-50%)
+        }
+        &.position-middle{
+            top: 50%;
+            transform: translate(-50%,-50%)
         }
     }
 </style>
