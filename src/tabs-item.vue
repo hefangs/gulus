1ae52075921c702b95e81f81390423369eaad500
hefang <he529564582@163.com>
2019-06-09 23:59:23 +0800

    继续完善tab样式 & 支持 disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,58 +1,65 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
-        <slot></slot>
-    </div>
+  <div class="tabs-item" @click="onClick" :class="classes">
+    <slot></slot>
+  </div>
 </template>
 <script>
-    export default {
-        name: 'GuluTabsItem',
-        inject: ['eventBus'],
-            data () {
-                return {
-                    active: false
-            }
-        },
-        props: {
-            disabled: {
-                type: Boolean,
-                default: false
-        },
-            name: {
-                type: String | Number,
-                required: true
-            }
-        },
-        computed: {
-            classes () {
-                return {
-                    active: this.active
-                }
-            }
-        },
-        created () {
-                this.eventBus.$on('update:selected', (name) => {
-                this.active = name === this.name;
-            })
-        },
-        methods: {
-            xxx () {
-                this.eventBus.$emit('update:selected', this.name, this)
-            }
+  export default {
+    name: 'GuluTabsItem',
+    inject: ['eventBus'],
+    data () {
+      return {
+        active: false
+      }
+    },
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      name: {
+        type: String | Number,
+        required: true
+      }
+    },
+    computed: {
+      classes () {
+        return {
+          active: this.active,
+          disabled: this.disabled
         }
+      }
+    },
+    created () {
+      this.eventBus.$on('update:selected', (name) => {
+        this.active = name === this.name;
+      })
+    },
+    methods: {
+      onClick () {
+          if (this.disabled) { return }
+        this.eventBus.$emit('update:selected', this.name, this)
+      }
     }
-    </script>
+  }
+</script>
 <style lang="scss" scoped>
-    $blue: blue;
-    .tabs-item {
-        flex-shrink: 0;
-        padding: 0 1em;
-        cursor: pointer;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        &.active {
-            color: $blue;
-            font-weight: bold;
-        }
+  $blue: blue;
+   $disabled-text-color: grey;
+  .tabs-item {
+    flex-shrink: 0;
+    padding: 0 1em;
+    cursor: pointer;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    &.active {
+      color: $blue;
+      font-weight: bold;
     }
+    &.disabled {
+      color: $disabled-text-color;
+      cursor:not-allowed;
+    }
+  }
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -1,54 +1,50 @@
 <template>
-    <div class="tabs">
-        <slot></slot>
-    </div>
+  <div class="tabs">
+    <slot></slot>
+  </div>
 </template>
 <script>
-import Vue from 'vue'
-    export default {
-       name:'GuluTabs',
-       props:{
-           selected:{
-               type:String,
-               required:true,
-           },
-           direction:{
-               type:String,
-               default:'horizontal',
-               validator(value){
-                   return ['horizontal','vertical'].indexOf(value) >= 0
-                }
-           }
-       },
-       data(){
-           return{
-               eventBus:new Vue()
-           }
-       },
-       provide(){
-           return{
-             eventBus:this.eventBus  
-           }
-       },
-       mounted(){
-           this.$children.forEach((vm) =>{
-               if(vm.$options.name ==='GuluTabsHead'){
-                   vm.$children.forEach((item) =>{
-                       if(item.$options.name === 'GuluTabsItem' && item.name ===this.selected){
-                           this.eventBus.$emit('update:selected',this.selected,item)
-                       }
-                   })
-               }
-           })
-       },
-       created(){
-        //    this.$emit('update:selected')
-       }
+  import Vue from 'vue'
+  export default {
+    name: 'GuluTabs',
+    props: {
+      selected: {
+        type: String,
+        required: true
+      },
+      direction: {
+        type: String,
+        default: 'horizontal',
+        validator (value) {
+          return ['horizontal', 'vertical'].indexOf(value) >= 0
+        }
+      }
+    },
+    data () {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide () {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted () {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'GuluTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'GuluTabsItem'
+              && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
     }
+  }
 </script>
-<style lang="scss" scoped>
-    .tabs{
-  
-    }
+<style>
+  .tabs {
+  }
 </style>
-
