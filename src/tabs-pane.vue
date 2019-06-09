574bf80a07d2019acc0fe21cbbda61090a93f10d
hefang <he529564582@163.com>
2019-06-09 19:00:22 +0800

    添加tabs 相关组件，添加基本props

---
+++ src/tabs-pane.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="tabs-pans">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+       name:'GuluTabsPane' 
+    }
+</script>
+<style lang="scss" scoped>
+    .tabs-pans{
+        
+    }
+</style>
