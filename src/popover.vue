535f118a5b456286eb08aa6c4a9a7dfa33e6199a
hefang <he529564582@163.com>
2019-06-10 22:09:12 +0800

    创建popover.vue

---
+++ src/popover.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="popover" @click="onClick">
+        <slot class="contentWrapper" name="content" v-if="visible"></slot>
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+       name: 'GuluPopover',
+       data(){
+           return{
+               visible:false,
+           }
+       },
+        methods:{
+            onClick(){
+                this.visible = !this.visible
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .popover{
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+        .contentWrapper{
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            border:1px solid red;
+            box-shadow: 0 0 3px rgba(0, 0, 0.5)
+        }
+    }
+</style>
