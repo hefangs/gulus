f5c7a6b6ef8b0bd936006f27ba66301ccf2b7a21
he529564582 <he529564582@163.com>
2019-06-13 20:24:46 +0800

    修改sass

--- src/button.vue
+++ src/button.vue
@@ -1,68 +1,66 @@
+    
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
     @click="$emit('click')">
-        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
-        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
-        <div class="content">
-            <slot></slot>
-        </div>
-    </button>
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"/>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+    <div class="g-button-content">
+      <slot/>
+    </div>
+  </button>
 </template>
 <script>
-    import Icon from './icon'
-    export default{
-        components:{
-            'g-icon':Icon
-        },
-       props:{
-          icon:{},
-          loading:{
-              type:Boolean,
-              default:false
-          },
-          iconPosition:{
-              type:String,
-              default:'left',
-              validator(e){
-                 return e === 'left' || e === 'right'
-              }
-          } 
-       }
+  import Icon from './icon'
+  export default {
+    name: 'GuluButton',
+    components: {
+      'g-icon': Icon
+    },
+    props: {
+      icon: {},
+      loading: {
+        type: Boolean,
+        default: false
+      },
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator (value) {
+          return value === 'left' || value === 'right'
+        }
+      }
     }
+  }
 </script>
 <style lang="scss" scoped>
-    @keyframes spin{
-        0%{transform: rotate(0deg)};
-        100%{transform: rotate(360deg)};
+  $font-size: 14px;
+  $button-height: 32px;
+  $button-bg: white;
+  $button-active-bg: #eee;
+  $border-radius: 4px;
+  $color: #333;
+  $border-color: #999;
+  $border-color-hover: #666;
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+  .g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
+    border-radius: $border-radius; border: 1px solid $border-color;
+    background: $button-bg;
+    display: inline-flex; justify-content: center; align-items: center;
+    vertical-align: middle;
+    &:hover { border-color: $border-color-hover; }
+    &:active { background-color: $button-active-bg; }
+    &:focus { outline: none; }
+    > .g-button-content { order: 2; }
+    > .icon { order: 1; margin-right: .1em; }
+    &.icon-right {
+      > .g-button-content { order: 1; }
+      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
     }
-    .g-button{
-        font-size: var(--font-size);
-        height:var(--button-height);
-        border-radius: var(--border-radius);
-        border:1px solid var(--border-color);
-        background:var(--button-background);
-        padding:0 1em;
-        vertical-align: middle;
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        &:hover{border-color: var(--border-color-hove)}
-        &:active{ background:var(--button-active-background) }
-        &:focus{outline: none;}
-        > .icon{order: 1;margin-right: 0.3em;}
-        > .content {order:2}
-        &.icon-right{
-            > .icon{
-                order:2;
-                margin-right: 0;
-                margin-left: 0.3em
-            }
-            > .content{
-                order:1;
-            }
-        }
-        .loading{
-            animation: spin 1s infinite linear;
-        }
+    .loading {
+      animation: spin 2s infinite linear;
     }
+  }
 </style>
