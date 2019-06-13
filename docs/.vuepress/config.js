33ffb2d5cf965a4f4fb53de301c97b5b0d1798c5
he529564582 <he529564582@163.com>
2019-06-13 16:35:55 +0800

    基于vuepress创建一个静态网站

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title: 'Wheels UI',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            {
+              text: 'Languages',
+              items: [
+                { text: '主页', items:'/' },
+                { text: '文档', items: '/guide/' },
+                { text: '交流', items: 'https://google.com' }
+              ]
+            }
+          ],
+        sidebar: [ 
+            {
+                title: '新手入门',
+                collapsable: false,
+                children: [
+                    '/install/',
+                    '/get-started/',
+                ]
+              },
+              {
+                title: '组件',
+                collapsable: false,
+                children: [
+                    '/component/button'
+                ]
+              },
+
+        ]
+      }
+  }
